65d580777eda56ecc7b41bc60f07076786028478
DEllster <sidkiyassi@gmail.com>
2021-02-05 10:32:13 +0100

    testClick not defined

--- src/public/client.js
+++ src/public/client.js
@@ -3,93 +3,201 @@ const { default: fetch } = require("node-fetch");
 
 const store = immutable.Map({
   user: immutable.Map({ name: "Student" }),
-  rovers: immutable.Map({
+  rovers: immutable.List(["curiosity", "opportunity", "spirit"]),
+  r: immutable.Map({
     curiosity: immutable.Map({ name: "Curiosity" }),
     opportunity: immutable.Map({ name: "Opportunity" }),
     spirit: immutable.Map({ name: "Spirit" }),
   }),
 });
 
-// const d = store.set("user", store.get("user").set("name", "Yassine"));
-// console.log(d.get("user").get("name"));
-// console.log(store.get("user").get("name"));
+const updateStore = (state, newState) => {
+  state = state.merge(state, newState);
+  render(root, state);
+};
 
-// add our markup to the page
-//const root = document.getElementById("root");
-
-// let newState = immutable.Map({
-//   user: immutable.Map({ name: "Yassine" }),
-//   rovers: immutable.List(["Curiosity", "YEah", "Spirit"]),
-// });
+//add our markup to the page
+const root = document.getElementById("root");
 
 const render = async (root, state) => {
   root.innerHTML = App(state);
 };
 
-// create content
-const App = (state) => {
-  let { rovers } = state;
-
-  return `
-        <header></header>
-        <main>
-            ${rovers.map((rover) => {
-              displayRover(rover);
-            })}
-            <section >
-            ${displayPhoto(rover)}
-            </section>
-        </main>
-        <footer></footer>
-    `;
+const testClick = () => {
+  alert("hello");
 };
+const header = (state) => {
+  return state
+    .get("rovers")
+    .map((item) => `<button onclick=testClick()>${item}</button>`)
+    .reduce((result, currentItem) => result + currentItem);
+};
+
+const App = (state) => completeUI(state, header, core);
+
+const completeUI = (state, header, core) => {
+  return `
+  <div>
+    ${header(state)}
+  </div>
+
+  <div>
+    ${core(state, getRoverInfos, getRoverPhotos)}
+  </div>
+
+  `;
+};
+
+const core = (state, getRoverInfos, getRoverPhotos) => {
+  return state.get("r").map(function (item) {
+    console.log(item.toJS());
+    return `<div>${getRoverInfos(
+      item
+    )}</div><div>${getRoverPhotos(item)}</div>`;
+  });
+};
+
+const getRoverInfos = (item) => {};
+
+const getRoverPhotos = (item) => {};
+
+const renderSelectedRoverInfos = (state) => {};
+
+const renderRoversInfos = (state) =>
+  state.get("rovers").map(function (rover) {
+    return `
+    <div>start section </div>
+    <section id = ${rover.get("id")}>
+              <h1>${rover.get("name")}</h1>
+              <h1>${rover.get("landing_date")}</h1>
+              <h1>${rover.get("launch_date")}</h1>
+              <h1>${rover.get("status")}</h1>
+              <h1>${rover.get("max_sol")}</h1>
+              <h1>${rover.get("total_photos")}</h1>
+    </section>`;
+  });
+
+//tested
+const displayRoverInfo = (rover) => {
+  let {
+    id,
+    name,
+    landing_date,
+    launch_date,
+    status,
+    max_sol,
+    total_photos,
+  } = rover;
+  return `
+          <section id = ${id}>
+              <h1>${name}</h1>
+              <h1>${landing_date}</h1>
+              <h1>${launch_date}</h1>
+              <h1>${status}</h1>
+              <h1>${max_sol}</h1>
+              <h1>${total_photos}</h1>
+          </section>
+      `;
+};
+
+//tested
+const updateRoversInfos = async (state, rover, callback) => {
+  console.log("rover ===", rover);
+  let { name } = rover;
+  console.log(name);
+  fetch(`http://localhost:3000/${name}`)
+    .then((res) => res.json())
+    .then(function (data) {
+      let {
+        id,
+        name,
+        landing_date,
+        launch_date,
+        status,
+        max_sol,
+        total_photos,
+      } = data["photos"]["rover"];
+      let newState = state.setIn(
+        ["rovers", name],
+        immutable.Map({
+          id,
+          name,
+          landing_date,
+          launch_date,
+          status,
+          max_sol,
+          total_photos,
+        })
+      );
+      callback(state, newState);
+    })
+    .catch((error) => console.log(error));
+};
+
+// // create content
+// const App = (state) => {
+//   //let rovers = state.get("rovers");
+//   //console.log(rovers.toJS());
+//   return `
+//         <header></header>
+//         <main>
+//             <div>
+//             ${test(state)}
+//             </div>
+
+//         </main>
+//         <footer></footer>
+//     `;
+// };
 
 // listening for load event because page should load before any JS is called
 window.addEventListener("load", () => {
   render(root, store);
 });
 
+completeUI(store);
+
 // ------------------------------------------------------  COMPONENTS
 
-// Pure function that renders conditional information -- THIS IS JUST AN EXAMPLE, you can delete it.
-const Greeting = (name) => {
-  if (name) {
-    return `
-            <h1>Welcome, ${name}!</h1>
-        `;
-  }
+// // Pure function that renders conditional information -- THIS IS JUST AN EXAMPLE, you can delete it.
+// const Greeting = (name) => {
+//   if (name) {
+//     return `
+//             <h1>Welcome, ${name}!</h1>
+//         `;
+//   }
 
-  return `
-        <h1>Hello!</h1>
-    `;
-};
+//   return `
+//         <h1>Hello!</h1>
+//     `;
+// };
 
-// Example of a pure function that renders infomation requested from the backend
-const ImageOfTheDay = (apod) => {
-  // If image does not already exist, or it is not from today -- request it again
-  const today = new Date();
-  const photodate = new Date(apod.date);
-  console.log(photodate.getDate(), today.getDate());
+// // Example of a pure function that renders infomation requested from the backend
+// const ImageOfTheDay = (apod) => {
+//   // If image does not already exist, or it is not from today -- request it again
+//   const today = new Date();
+//   const photodate = new Date(apod.date);
+//   console.log(photodate.getDate(), today.getDate());
 
-  console.log(photodate.getDate() === today.getDate());
-  if (!apod || apod.date === today.getDate()) {
-    getImageOfTheDay(store);
-  }
+//   console.log(photodate.getDate() === today.getDate());
+//   if (!apod || apod.date === today.getDate()) {
+//     getImageOfTheDay(store);
+//   }
 
-  // check if the photo of the day is actually type video!
-  if (apod.media_type === "video") {
-    return `
-            <p>See today's featured video <a href="${apod.url}">here</a></p>
-            <p>${apod.title}</p>
-            <p>${apod.explanation}</p>
-        `;
-  } else {
-    return `
-            <img src="${apod.image.url}" height="350px" width="100%" />
-            <p>${apod.image.explanation}</p>
-        `;
-  }
-};
+//   // check if the photo of the day is actually type video!
+//   if (apod.media_type === "video") {
+//     return `
+//             <p>See today's featured video <a href="${apod.url}">here</a></p>
+//             <p>${apod.title}</p>
+//             <p>${apod.explanation}</p>
+//         `;
+//   } else {
+//     return `
+//             <img src="${apod.image.url}" height="350px" width="100%" />
+//             <p>${apod.image.explanation}</p>
+//         `;
+//   }
+// };
 
 // ------------------------------------------------------  API CALLS
 
@@ -103,65 +211,3 @@ const ImageOfTheDay = (apod) => {
 
 //   return data;
 // };
-
-const getRoverInfos = (state, rover) => {
-  let rovers = state.get("rovers");
-  if (rovers.includes(rover)) {
-  }
-
-  return {};
-};
-
-//tested
-const displayRover = (rover) => {
-  let {
-    id,
-    name,
-    landing_date,
-    launch_date,
-    status,
-    max_sol,
-    total_photos,
-  } = rover.toJS();
-  return `
-          <section id = ${id}>
-              <h1>${name}</h1>
-              <h1>${landing_date}</h1>
-              <h1>${launch_date}</h1>
-              <h1>${status}</h1>
-              <h1>${max_sol}</h1>
-              <h1>${total_photos}</h1>
-          </section>
-      `;
-};
-//to test
-const updateStore = (state, newState) => {
-  state = state.merge(state, newState);
-  render(root, state);
-};
-
-//tested
-const getRoverInfos = async (state, rover /*, action*/) => {
-  fetch(`http://localhost:3000/${rover}`)
-    .then((res) => res.json())
-    .then(function (data) {
-      let {
-        name,
-        landing_date,
-        launch_date,
-        status,
-        max_sol,
-        total_photos,
-      } = data["photos"]["rover"];
-      let newState = state.setIn(["rovers", rover], {
-        name,
-        landing_date,
-        launch_date,
-        status,
-        max_sol,
-        total_photos,
-      });
-      updateStore(state, newState);
-    })
-    .catch((error) => console.log(error));
-};
--- test.js
+++ test.js
@@ -1,56 +1,74 @@
-const immutable = require("immutable");
-const { default: fetch } = require("node-fetch");
+// const immutable = require("immutable");
+// const { default: fetch } = require("node-fetch");
 
-const store = immutable.Map({
-  user: immutable.Map({ name: "Student" }),
-  rovers: immutable.Map({
-    curiosity: immutable.Map({ name: "Curiosity" }),
-    opportunity: immutable.Map({ name: "Opportunity" }),
-    spirit: immutable.Map({ name: "Spirit" }),
-  }),
-});
+// let store = immutable.Map({
+//   user: immutable.Map({ name: "Student" }),
+//   r: immutable.List(["curiosity", "opportunity", "spirit"]),
+//   rovers: immutable.Map({
+//     curiosity: immutable.Map({ name: "Curiosity" }),
+//     opportunity: immutable.Map({ name: "Opportunity" }),
+//     spirit: immutable.Map({ name: "Spirit" }),
+//   }),
+// });
 
-const updateStore = (state, newState) => {
-  state = state.merge(state, newState);
-  console.log(state.toJS());
+// let rovers = store.get("r");
+
+// rovers.map((r) => console.log(r));
+
+// store = store.merge(store, {
+//   r: immutable.List(["test1", "test2", "test3"]),
+// });
+
+// console.log(store.toJS());
+
+
+// // const updateStore = (state, newState) => {
+// //   state = state.merge(state, newState);
+// //   console.log(state.toJS());
+// // };
+
+// // const getRoverInfos = async (state, rover /*, action*/) => {
+// //   fetch(`http://localhost:3000/${rover}`)
+// //     .then((res) => res.json())
+// //     .then(function (data) {
+// //       let {
+// //         name,
+// //         landing_date,
+// //         launch_date,
+// //         status,
+// //         max_sol,
+// //         total_photos,
+// //       } = data["photos"]["rover"];
+// //       let newState = state.setIn(["rovers", rover], {
+// //         name,
+// //         landing_date,
+// //         launch_date,
+// //         status,
+// //         max_sol,
+// //         total_photos,
+// //       });
+// //       updateStore(state, newState);
+// //     })
+// //     .catch((error) => console.log(error));
+// // };
+
+// // const f = (state, rover) => {
+// //   let rovers = state.get("rovers");
+// //   if (rovers.includes(rover)) {
+// //     getRoverInfos(state, rover);
+// //   }
+// // };
+
+// // // //f(store, "Curiosity");
+
+// // // getRoverInfos(store, "Spirit");
+
+// // console.log(displayRover(store, "spirit"));
+
+const testClick = () => {
+  return () => {
+    console.log("test");
+  };
 };
 
-const getRoverInfos = async (state, rover /*, action*/) => {
-  fetch(`http://localhost:3000/${rover}`)
-    .then((res) => res.json())
-    .then(function (data) {
-      let {
-        name,
-        landing_date,
-        launch_date,
-        status,
-        max_sol,
-        total_photos,
-      } = data["photos"]["rover"];
-      let newState = state.setIn(["rovers", rover], {
-        name,
-        landing_date,
-        launch_date,
-        status,
-        max_sol,
-        total_photos,
-      });
-      updateStore(state, newState);
-    })
-    .catch((error) => console.log(error));
-};
-
-const f = (state, rover) => {
-  let rovers = state.get("rovers");
-  if (rovers.includes(rover)) {
-    getRoverInfos(state, rover);
-  }
-};
-
-// //f(store, "Curiosity");
-
-// getRoverInfos(store, "Spirit");
-
-
-
-console.log(displayRover(store, "spirit"));
+console.log(testClick()());
